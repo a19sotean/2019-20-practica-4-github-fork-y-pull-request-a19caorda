e60b1a11b5d868d07b68d241ed1ced4505d60469
David Castilla Ortiz <im@dcxo.dev>
2020-02-12 19:56:08 +0100

    a19caorda - Added Main.java file

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        string str = scan.nextLine();
+
+        System.out.printf("Hola %s", str)
+
+    }
+
+}
