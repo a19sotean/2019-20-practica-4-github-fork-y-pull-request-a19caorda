7ca600601ee4eaf517f449b82ee3ca55bd896303
David Castilla Ortiz <im@dcxo.dev>
2020-02-12 20:12:30 +0100

    a19caorda Added semicolon

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+
         Scanner scan = new Scanner(System.in);
         string str = scan.nextLine();
 
-        System.out.printf("Hola %s", str)
+        System.out.printf("Hola %s", str);
 
     }
 
